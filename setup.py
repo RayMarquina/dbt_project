3c7377914e899463df2b331ad57cae47ce07dada
Drew Banin <dsb59@drexel.edu>
2016-03-23 11:48:17 -0400

    updates setup file

--- setup.py
+++ setup.py
@@ -3,17 +3,21 @@ from setuptools import setup, find_packages
 import os.path
 
 package_name = "dbt"
-package_version = "0.1.0-SNAPSHOT"
+package_version = "0.1.1"
 
 setup(
   name=package_name,
   version=package_version,
+  description="Data build tool for Analyst Collective",
+  author="Analyst Collective",
+  author_email="admin@analystcollective.org",
+  url="https://github.com/analyst-collective/dbt",
   packages=find_packages(),
   scripts=[
     'scripts/dbt',
   ],
   install_requires=[
-    'argparse>=1.2.1',
+    #'argparse>=1.2.1',
     'Jinja2>=2.8',
     'PyYAML>=3.11',
     'psycopg2==2.6.1',
