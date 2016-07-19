596e42642779e961846649eee2031f8b3027551d
Drew Banin <drew@8tracks.com>
2016-07-18 22:49:57 -0400

    finish compilation refactor

--- dbt/compilation.py
+++ dbt/compilation.py
@@ -4,6 +4,7 @@ import fnmatch
 import jinja2
 from collections import defaultdict
 import dbt.project
+from dbt.source import Source
 
 import networkx as nx
 
@@ -44,7 +45,7 @@ class Compiler(object):
         if not os.path.exists(self.project['modules-path']):
             os.makedirs(self.project['modules-path'])
 
-    def __dependency_projects(self):
+    def dependency_projects(self):
         for obj in os.listdir(self.project['modules-path']):
             full_obj = os.path.join(self.project['modules-path'], obj)
             if os.path.isdir(full_obj):
@@ -52,204 +53,104 @@ class Compiler(object):
                 yield project
 
 
-    def __walk_dir_for_sources(self, project, paths):
-        """returns: {'model': ['pardot/model.sql', 'segment/model.sql']}
-        """
-        indexed_files = defaultdict(list)
+    def model_sources(self, project):
+        "source_key is a dbt config key like source-paths or analysis-paths"
+        paths = project.get('source-paths', [])
+        return Source(project).get_models(paths)
 
-        for source_path in paths:
-            full_source_path = os.path.join(project['project-root'], source_path)
-            for root, dirs, files in os.walk(full_source_path):
-                for filename in files:
-                    abs_path = os.path.join(root, filename)
-                    rel_path = os.path.relpath(abs_path, full_source_path)
+    def analysis_sources(self, project):
+        "source_key is a dbt config key like source-paths or analysis-paths"
+        paths = project.get('analysis-paths', [])
+        return Source(project).get_analyses(paths)
 
-                    if fnmatch.fnmatch(filename, "[!.#~]*.sql"):
-                        indexed_files[full_source_path].append((project, rel_path))
+    def validate_models_unique(self, models):
+        model_names = set()
+        for model in models:
+            if model.name in model_names:
+                # TODO : Package?
+                raise RuntimeError("ERROR: Conflicting model found model={}".format(model.name))
+            else:
+                model_names.add(model.name)
 
-        return indexed_files
-
-    def __project_sources(self, project):
-      source_paths = project.get('source-paths', [])
-      return self.__walk_dir_for_sources(project, source_paths)
-
-    def __project_analyses(self, project):
-      source_paths = project.get('analysis-paths', [])
-      return self.__walk_dir_for_sources(project, source_paths)
-
-    def __project_models(self, project_sources):
-        project_models = []
-        for (source_path, model_definitions) in project_sources.items():
-
-            for (project, model_file) in model_definitions:
-
-                package_name = project.cfg['name']
-
-                filename = os.path.basename(model_file)
-                model_group = os.path.dirname(model_file)
-                model_name  = os.path.splitext(filename)[0]
-                model = (package_name, model_group, model_name)
-
-                if model in project_models:
-                    print("WARNING: Conflicting model found package={}, model={}".format(package_name, model_name))
-
-                # add the conflict and catch it if it's used in compilation
-                project_models.append(model)
-
-        return project_models
-
-
-    def __write(self, original_path, payload, target_suffix=""):
-        dirname = os.path.dirname(original_path)
-        filename = os.path.basename(original_path)
-        out_path = os.path.join(dirname, self.create_template.model_name(filename))
-
-        target_dir = self.create_template.label + target_suffix
-        target_path = os.path.join(self.project['target-path'], target_dir, out_path)
+    def __write(self, build_filepath, payload):
+        target_path = os.path.join(self.project['target-path'], build_filepath)
 
         if not os.path.exists(os.path.dirname(target_path)):
             os.makedirs(os.path.dirname(target_path))
 
+        print target_path
+
         with open(target_path, 'w') as f:
             f.write(payload)
-
-    def __sort_qualifier(self, model_config):
-        sort_keys = model_config['sort']
-        if type(sort_keys) == str:
-            sort_keys = [sort_keys]
-
-        # remove existing quotes in field name, then wrap in quotes
-        formatted_sort_keys = ['"{}"'.format(sort_key.replace('"', '')) for sort_key in sort_keys]
-        return "sortkey ({})".format(', '.join(formatted_sort_keys))
-
-    def __dist_qualifier(self, model_config):
-        dist_key = model_config['dist']
-
-        if type(dist_key) != str:
-            raise RuntimeError("The provided distkey '{}' is not valid!".format(dist_key))
-
-        return 'distkey ("{}")'.format(dist_key)
-
-    def __wrap_in_create(self, model_name, query, model_config):
-
-        # default to view if not provided in config!
-        table_or_view = 'table' if model_config['materialized'] else 'view'
-
-        ctx = self.project.context()
-        schema = ctx['env'].get('schema', 'public')
-
-        dist_qualifier = ""
-        sort_qualifier = ""
-
-        if table_or_view == 'table':
-            if 'dist' in model_config:
-                dist_qualifier = self.__dist_qualifier(model_config)
-            if 'sort' in model_config:
-                sort_qualifier = self.__sort_qualifier(model_config)
-
-        opts = {
-            "table_or_view": table_or_view,
-            "schema": schema,
-            "identifier": model_name,
-            "query": query,
-            "dist_qualifier": dist_qualifier,
-            "sort_qualifier": sort_qualifier
-        }
-
-        return self.create_template.wrap(opts)
 
     def __get_model_identifiers(self, model_filepath):
         model_group = os.path.dirname(model_filepath)
         model_name, _ = os.path.splitext(os.path.basename(model_filepath))
         return model_group, model_name
 
-    def get_model_config(self, model_group, model_name):
-        """merges model, model group, and base configs together. Model config
-        takes precedence, then model_group, then base config"""
-
-        config = self.project['model-defaults'].copy()
-
-        model_configs = self.project['models']
-        model_group_config = model_configs.get(model_group, {})
-        model_config = model_group_config.get(model_name, {})
-
-        config.update(model_group_config)
-        config.update(model_config)
-
-        return config
-
     def find_model_by_name(self, project_models, name, package_namespace=None):
         found = []
         for model in project_models:
-            package, model_group, model_name = model
-            if model_name == name:
+            if model.name == name:
                 if package_namespace is None:
                     found.append(model)
-                elif package_namespace is not None and package_namespace == package:
+                elif package_namespace is not None and package_namespace == model.project['name']:
                     found.append(model)
 
+        nice_package_name = 'ANY' if package_namespace is None else package_namespace
         if len(found) == 0:
-            nice_package_name = 'ANY' if package_namespace is None else package_namespace
             raise RuntimeError("Can't find a model named '{}' in package '{}' -- does it exist?".format(name, nice_package_name))
         elif len(found) == 1:
             return found[0]
         else:
-            raise RuntimeError("Model specification is ambiguous: model='{}' package='{}' -- {} models match criteria".format(name, package_namespace, len(found)))
+            raise RuntimeError("Model specification is ambiguous: model='{}' package='{}' -- {} models match criteria: {}".format(name, nice_package_name, len(found), found))
 
-    def __ref(self, linker, ctx, source_model, project_models):
+    def __ref(self, linker, ctx, model, all_models):
         schema = ctx['env']['schema']
 
         # if this node doesn't have any deps, still make sure it's a part of the graph
+        source_model = tuple(model.fqn)
         linker.add_node(source_model)
 
         def do_ref(*args):
             if len(args) == 1:
                 other_model_name = args[0]
-                other_model = self.find_model_by_name(project_models, other_model_name)
+                other_model = self.find_model_by_name(all_models, other_model_name)
             elif len(args) == 2:
                 other_model_package, other_model_name = args
-                other_model = self.find_model_by_name(project_models, other_model_name, package_namespace=other_model_package)
+                other_model = self.find_model_by_name(all_models, other_model_name, package_namespace=other_model_package)
 
             other_model_name = self.create_template.model_name(other_model_name)
-            other_model = (other_model[0], other_model[1], self.create_template.model_name(other_model[2]))
 
-            linker.dependency(source_model, other_model)
+            # TODO : wtf is up here?
+            other_model_fqn = tuple(other_model.fqn[:-1] + [other_model_name])
+
+            linker.dependency(source_model, other_model_fqn)
             return '"{}"."{}"'.format(schema, other_model_name)
 
         return do_ref
 
-    def __do_compile(self, src_index, project_models, make_create=True, include=[]):
-        linker = Linker()
+    def compile_model(self, linker, model, models):
+        jinja = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath=model.top_dir))
 
-        created = 0
-        for src_path, project_files in src_index.items():
-            jinja = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath=src_path))
-            for (project, f) in project_files:
-                model_group, model_name = self.__get_model_identifiers(f)
-                model_name = self.create_template.model_name(model_name)
-                model_config = self.get_model_config(model_group, model_name)
+        model_config = model.get_config(self.project)
 
-                if not model_config.get('enabled') and model_name not in include:
-                    continue
+        if not model_config.get('enabled'):
+            return None
 
-                template = jinja.get_template(f)
+        template = jinja.get_template(model.rel_filepath)
 
-                context = self.project.context()
-                source_model = (project['name'], model_group, model_name)
-                context['ref'] = self.__ref(linker, context, source_model, project_models)
+        context = self.project.context()
+        context['ref'] = self.__ref(linker, context, model, models)
 
-                rendered = template.render(context)
+        rendered = template.render(context)
 
-                if make_create:
-                  create_stmt = self.__wrap_in_create(model_name, rendered, model_config)
-                  if create_stmt:
-                      self.__write(f, create_stmt)
-                      created += 1
-                else:
-                  self.__write(f, rendered, "-analysis")
-                  created += 1
-
-        return linker, created
+        stmt = model.compile(rendered, self.project, self.create_template)
+        if stmt:
+            build_path = model.build_path(self.create_template)
+            self.__write(build_path, stmt)
+            return True
+        return False
 
     def __write_graph_file(self, linker):
         filename = 'graph-{}.yml'.format(self.create_template.label)
@@ -257,16 +158,29 @@ class Compiler(object):
         linker.write_graph(graph_path)
 
     def compile(self):
-        sources = self.__project_sources(self.project)
+        models = self.model_sources(self.project)
 
-        for project in self.__dependency_projects():
-            sources.update(self.__project_sources(project))
+        for project in self.dependency_projects():
+            models.extend(self.model_sources(project))
 
-        project_models = self.__project_models(sources)
-        model_linker, model_created = self.__do_compile(sources, project_models)
+        self.validate_models_unique(models)
+
+        model_linker = Linker()
+        compiled_models = []
+        for model in models:
+            compiled = self.compile_model(model_linker, model, models)
+            if compiled:
+                compiled_models.append(compiled)
+
         self.__write_graph_file(model_linker)
 
-        analysis_sources = self.__project_analyses(self.project)
-        analysis_linker, analysis_created = self.__do_compile(analysis_sources, project_models, make_create=False)
+        analysis_linker = Linker()
+        analyses = self.analysis_sources(self.project)
+        compiled_analyses = []
+        for analysis in analyses:
+            compiled = self.compile_model(analysis_linker, analysis, models)
+            if compiled:
+                compiled_analyses.append(compiled)
 
-        return model_created, analysis_created
+        return len(compiled_models), len(compiled_analyses)
+
--- dbt/source.py
+++ dbt/source.py
@@ -1,11 +1,13 @@
 
 import os.path
 import fnmatch
-from dbt.model import Model, CompiledModel, Schema
+from dbt.model import Model, Analysis, CompiledModel, Schema
 
-class Source():
-    def __init__(self, project_root):
-        self.project_root = project_root
+class Source(object):
+    def __init__(self, project):
+        self.project = project
+        self.project_root = project['project-root']
+        self.project_name = project['name']
 
     def find(self, source_paths, file_pattern):
         """returns abspath, relpath, filename of files matching file_regex in source_paths"""
@@ -18,42 +20,33 @@ class Source():
                     rel_path = os.path.relpath(abs_path, root_path)
 
                     if fnmatch.fnmatch(filename, file_pattern):
-                        found.append((source_path, rel_path))
+                        found.append((self.project, source_path, rel_path))
         return found
 
     def get_models(self, model_dirs):
         pattern = "[!.#~]*.sql"
-        models = [Model(*paths) for paths in self.find(model_dirs, pattern)]
+        models = [Model(*model) for model in self.find(model_dirs, pattern)]
+        return models
+
+    def get_analyses(self, analysis_dirs):
+        pattern = "[!.#~]*.sql"
+        models = [Analysis(*analysis) for analysis in self.find(analysis_dirs, pattern)]
         return models
 
     def get_compiled(self, target_dir, compilation_type):
         "Get compiled SQL files. compilation_type E {build, test}"
+        if compilation_type not in ['build', 'test']:
+            raise RuntimeError('Invalid compilation_type. Must be on of ["build", "test]. Got {}'.format(compilation_type))
         pattern = "[!.#~]*.sql"
         source_dir = os.path.join(target_dir, compilation_type)
-        compiled_models = [CompiledModel(*paths) for paths in self.find([source_dir], pattern)]
+        compiled_models = [CompiledModel(*model) for model in self.find([source_dir], pattern)]
         return compiled_models
 
     def get_schemas(self, model_dirs):
         "Get schema.yml files"
         pattern = "[!.#~]*.yml"
-        schemas = [Schema(*paths) for paths in self.find(model_dirs, pattern)]
+        schemas = [Schema(*schema) for schema in self.find(model_dirs, pattern)]
         return schemas
 
 
-# compilation.py: map of abspath to model root --> (project, model_rel_path)
-# args: project, paths
-#['/Users/Drew/Desktop/analyst_collective/test-dbt/models'] --> 'idk/people.sql'
 
-# runner.py: list of rel paths to compiled files
-# args: none
-#['idk/accounts.sql', 'idk/people.sql', 'idk/people_accounts.sql'] 
-
-# schema_tester.py: map of model root --> parsed yaml file
-# args: none
-#{'idk': {'accounts': {'constraints': {'accepted-values': [{'field': 'type', 'values': ['paid', 'free']}]}}, 'people': {'constraints': {'relationships': [{'to': 'accounts', 'from': 'account_id', 'field': 'id'}], 'not_null': ['id', 'account_id', 'name'], 'unique': ['id'], 'accepted-values': [{'field': 'type', 'values': ['paid', 'free']}]}}}}
-
-# seeder.py: list of abs path to CSV files in specified dir
-# args: none
-#['/Users/Drew/Desktop/analyst_collective/test-dbt/data/iris3.csv']
-
-# source.py:
