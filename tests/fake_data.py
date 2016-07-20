eea31b92816ebdf6087d0ea8e68b880195049643
Drew Banin <drew@8tracks.com>
2016-07-20 12:50:00 -0400

    start adding compilation tests

---
+++ tests/fake_data.py
@@ -0,0 +1,19 @@
+
+import dbt.project
+from dbt.compilation import Compiler
+from dbt.templates import BaseCreateTemplate, TestCreateTemplate
+
+def new_project():
+    return dbt.project.read_project('tests/test-project/dbt_project.yml')
+
+def new_base_compiler():
+    project = new_project()
+    create_template = BaseCreateTemplate
+    compiler = Compiler(project, create_template)
+    return compiler
+
+def new_test_compiler():
+    project = new_project()
+    create_template = TestCreateTemplate
+    compiler = Compiler(project, create_template)
+    return compiler
