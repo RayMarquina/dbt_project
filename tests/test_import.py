1abb04646c77c76c2c396f942b52b53794362d82
Drew Banin <drew@8tracks.com>
2016-07-19 23:59:52 -0400

    add linker tests

--- tests/test_import.py
+++ tests/test_import.py
@@ -2,62 +2,7 @@ import unittest
 
 class ImportTest(unittest.TestCase):
 
-    def test_import_dbt_project(self):
-        import dbt.project
-
-    def test_import_dbt_task_run(self):
-        import dbt.task.run
-
-    def test_import_dbt_task_compile(self):
-        import dbt.task.compile
-
-    def test_import_dbt_task_debug(self):
-        import dbt.task.debug
-
-    def test_import_dbt_task_clean(self):
-        import dbt.task.clean
-
-    def test_import_dbt_task_deps(self):
-        import dbt.task.deps
-
-    def test_import_dbt_task_init(self):
-        import dbt.task.init
-
-    def test_import_dbt_task_seed(self):
-        import dbt.task.seed
-
-    def test_import_dbt_task_test(self):
-        import dbt.task.test
-
     def test_import_dbt_main(self):
+        "just test that the project can be imported"
         import dbt.main
 
-    def test_import_dbt_project(self):
-        import dbt.project
-
-    def test_import_dbt_schema_tester(self):
-        import dbt.schema_tester
-
-    def test_import_dbt_source(self):
-        import dbt.source
-
-    def test_import_dbt_templates(self):
-        import dbt.templates
-
-    def test_import_dbt_compilation(self):
-        import dbt.compilation
-
-    def test_import_dbt_model(self):
-        import dbt.model
-
-    def test_import_dbt_runner(self):
-        import dbt.runner
-
-    def test_import_dbt_seeder(self):
-        import dbt.seeder
-
-    def test_import_dbt_targets(self):
-        import dbt.targets
-
-    def test_import_dbt_utils(self):
-        import dbt.utils
---
+++ tests/test_linker.py
@@ -0,0 +1,69 @@
+import unittest
+
+from dbt.compilation import Linker
+
+class LinkerTest(unittest.TestCase):
+
+    def setUp(self):
+        self.linker = Linker()
+
+    def test_linker_add_node(self):
+        expected_nodes = ['A', 'B', 'C']
+        for node in expected_nodes:
+            self.linker.add_node(node)
+
+        actual_nodes = self.linker.nodes()
+        for node in expected_nodes:
+            self.assertIn(node, actual_nodes)
+
+        self.assertEqual(len(actual_nodes), len(expected_nodes))
+
+    def test_linker_add_dependency(self):
+        actual_deps = [('A', 'B'), ('A', 'C'), ('B', 'C')]
+
+        for (l, r) in actual_deps:
+            self.linker.dependency(l, r)
+
+        expected_dep_list = ['C', 'B', 'A']
+        actual_dep_list = self.linker.as_dependency_list()
+        self.assertEqual(expected_dep_list, actual_dep_list)
+
+    def test_linker_add_disjoint_dependencies(self):
+        actual_deps = [('A', 'B')]
+        additional_node = 'Z'
+
+        for (l, r) in actual_deps:
+            self.linker.dependency(l, r)
+        self.linker.add_node(additional_node)
+
+        # has to be one of these three
+        possible = [
+                ['Z', 'B', 'A'],
+                ['B', 'A', 'Z'],
+                ['B', 'Z', 'A'],
+        ]
+
+        actual = self.linker.as_dependency_list()
+
+        for expected in possible:
+            if expected == actual:
+                return
+        self.assertTrue(False, actual)
+
+    def test_linker_dependencies_limited_to_some_nodes(self):
+        actual_deps = [('A', 'B'), ('B', 'C'), ('C', 'D')]
+
+        for (l, r) in actual_deps:
+            self.linker.dependency(l, r)
+
+        actual_limit = self.linker.as_dependency_list(['B'])
+        expected_limit = ['B', 'A']
+        self.assertEqual(expected_limit, actual_limit)
+
+    def test_linker_bad_limit_throws_runtime_error(self):
+        actual_deps = [('A', 'B'), ('B', 'C'), ('C', 'D')]
+
+        for (l, r) in actual_deps:
+            self.linker.dependency(l, r)
+
+        self.assertRaises(RuntimeError, self.linker.as_dependency_list, ['ZZZ'])
