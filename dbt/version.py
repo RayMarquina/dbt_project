5c32c4f84048a4b3350cf7e25cebfc5d634aec46
Drew Banin <drew@8tracks.com>
2016-10-21 10:36:46 -0400

    Bump version: 0.5.0 → 0.5.1

--- dbt/version.py
+++ dbt/version.py
@@ -53,7 +53,7 @@ def get_version_information():
 def is_latest():
     return installed == latest
 
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 installed = get_version()
 latest = get_latest_version()
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 import os.path
 
 package_name = "dbt"
-package_version = "0.5.0"
+package_version = "0.5.1"
 
 setup(
   name=package_name,
