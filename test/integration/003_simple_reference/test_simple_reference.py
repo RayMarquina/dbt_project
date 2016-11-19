ca0195f15a82b714b6379f9e877783720b879648
Drew Banin <drew@fishtownanalytics.com>
2016-11-19 15:26:01 -0500

    supply --models to dbt run

--- test/integration/003_simple_reference/test_simple_reference.py
+++ test/integration/003_simple_reference/test_simple_reference.py
@@ -1,6 +1,6 @@
 from test.integration.base import DBTIntegrationTest
 
-class TestSimpleDependency(DBTIntegrationTest):
+class TestSimpleReference(DBTIntegrationTest):
 
     def setUp(self):
         DBTIntegrationTest.setUp(self)
@@ -43,3 +43,34 @@ class TestSimpleDependency(DBTIntegrationTest):
         self.assertTablesEqual("summary_expected","materialized_summary")
         self.assertTablesEqual("summary_expected","view_summary")
         self.assertTablesEqual("summary_expected","ephemeral_summary")
+
+    def test_simple_reference_with_models(self):
+        # Run materialized_copy, ephemeral_copy, and their dependents
+        # ephemeral_copy should not actually be materialized b/c it is ephemeral
+        # the dependent ephemeral_summary, however, should be materialized as a table
+        self.run_dbt(['run', '--models', 'materialized_copy', 'ephemeral_copy'])
+
+        # Copies should match
+        self.assertTablesEqual("seed","materialized_copy")
+
+        # Summaries should match
+        self.assertTablesEqual("summary_expected","materialized_summary")
+        self.assertTablesEqual("summary_expected","ephemeral_summary")
+
+        created_models = self.get_models_in_schema()
+
+        self.assertFalse('incremental_copy' in created_models)
+        self.assertFalse('incremental_summary' in created_models)
+        self.assertFalse('view_copy' in created_models)
+        self.assertFalse('view_summary' in created_models)
+
+        # make sure this wasn't errantly materialized
+        self.assertFalse('ephemeral_copy' in created_models)
+
+        self.assertTrue('materialized_copy' in created_models)
+        self.assertTrue('materialized_summary' in created_models)
+        self.assertEqual(created_models['materialized_copy'], 'table')
+        self.assertEqual(created_models['materialized_summary'], 'table')
+
+        self.assertTrue('ephemeral_summary' in created_models)
+        self.assertEqual(created_models['ephemeral_summary'], 'table')
