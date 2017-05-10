d232e951cb1a4235f5fd9854e367fac8bfa1b26c
Drew Banin <drew@fishtownanalytics.com>
2017-05-10 11:38:41 -0400

    don't show a stacktrace when throwing compiler error (#420)
    * don't show a stacktrace when throwing compiler error for missing vars

--- dbt/utils.py
+++ dbt/utils.py
@@ -82,7 +82,7 @@ def compiler_warning(model, msg):
 class Var(object):
     UndefinedVarError = "Required var '{}' not found in config:\nVars "\
                         "supplied to {} = {}"
-    NoneVarError = "Supplied var '{}' is undefined in config:\nVars supplied"\
+    NoneVarError = "Supplied var '{}' is undefined in config:\nVars supplied "\
                    "to {} = {}"
 
     def __init__(self, model, context):
@@ -112,10 +112,11 @@ class Var(object):
         elif var_name in self.local_vars:
             raw = self.local_vars[var_name]
             if raw is None:
+                model_name = get_model_name_or_none(self.model)
                 compiler_error(
                     self.model,
                     self.NoneVarError.format(
-                        var_name, self.model.nice_name, pretty_vars
+                        var_name, model_name, pretty_vars
                     )
                 )
 
