34d0e33e9563b50196fd3945d466883f738601ce
Drew Banin <drew@fishtownanalytics.com>
2017-05-22 08:13:14 -0400

    bump some deps (#431)

--- setup.py
+++ setup.py
@@ -32,13 +32,13 @@ setup(
     install_requires=[
         'Jinja2>=2.8',
         'PyYAML>=3.11',
-        'psycopg2==2.6.2',
-        'sqlparse==0.1.19',
+        'psycopg2==2.7.1',
+        'sqlparse==0.2.3',
         'networkx==1.11',
         'csvkit==0.9.1',
         'snowplow-tracker==0.7.2',
         'celery==3.1.23',
-        'voluptuous==0.9.3',
-        'snowflake-connector-python==1.3.15',
+        'voluptuous==0.10.5',
+        'snowflake-connector-python==1.3.16',
     ],
 )
