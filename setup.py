1227c4b0a80523a99f72efc42d9cbc0fe1f40c77
Connor McArthur <cmcarthur@rjmetrics.com>
2017-07-28 17:46:00 -0400

    fix snowflake version for brew install --HEAD

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         'snowplow-tracker==0.7.2',
         'celery==3.1.23',
         'voluptuous==0.10.5',
-        'snowflake-connector-python==1.4.0',
+        'snowflake-connector-python>=1.3.16',
         'colorama==0.3.9',
         'google-cloud-bigquery==0.24.0',
         'pyasn1==0.2.3',
