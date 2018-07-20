743fef66d89dd2c9bbc605c69083cf832c174ad8
Connor McArthur <connor@fishtownanalytics.com>
2018-07-20 08:38:44 -0400

    fix version identifier for semver code

--- dbt/version.py
+++ dbt/version.py
@@ -67,6 +67,6 @@ def get_version_information():
                 .format(version_msg))
 
 
-__version__ = '0.10.2a1'
+__version__ = '0.10.2-a1'
 installed = get_installed_version()
 latest = get_latest_version()
