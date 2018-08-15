62228291ab8418e7c55402c765eee1ed0417a7e7
Jacob Beck <jake@fishtownanalytics.com>
2018-08-15 07:55:17 -0600

    PR feedback

--- dbt/include/global_project/macros/schema_tests/relationships.sql
+++ dbt/include/global_project/macros/schema_tests/relationships.sql
@@ -3,15 +3,16 @@
 
 {% set column_name = kwargs.get('column_name', kwargs.get('from')) %}
 
+
 select count(*)
 from (
-    select
-        model.{{ column_name }} as id
-    from {{ model }} model
-    left join {{ to }} target on target.{{ field }} = model.{{ column_name }}
-    where
-       model.{{ column_name }} is not null and
-       target.{{ field }} is null
-) validation_errors
+    select {{ column_name }} as id from {{ model }}
+) as child
+left join (
+    select {{ field }} as id from {{ to }}
+) as parent on parent.id = child.id
+where child.id is not null
+  and parent.id is null
 
 {% endmacro %}
+
