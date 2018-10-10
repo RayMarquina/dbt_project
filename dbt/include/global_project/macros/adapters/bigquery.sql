e6f044c516d8f0820784d817f4d0a74c29888e98
Jeremy Cohen <jeremy@fishtownanalytics.com>
2018-10-10 18:23:43 -0400

    make jinja respect whitespace

--- dbt/include/global_project/macros/adapters/bigquery.sql
+++ dbt/include/global_project/macros/adapters/bigquery.sql
@@ -14,7 +14,7 @@
 {% macro cluster_by(raw_cluster_by) %}
   {%- if raw_cluster_by is not none -%}
   cluster by 
-  {%- if raw_cluster_by is string -%}
+  {% if raw_cluster_by is string -%}
     {% set raw_cluster_by = [raw_cluster_by] %}
   {%- endif -%}
   {%- for cluster in raw_cluster_by -%}
