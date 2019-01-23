67d85316ac66244731e99af86107f134ee9481e5
Jacob Beck <jake@fishtownanalytics.com>
2019-01-23 13:47:34 -0700

    make the test a bit sillier

--- test/integration/042_sources_test/test_sources.py
+++ test/integration/042_sources_test/test_sources.py
@@ -13,6 +13,7 @@ class TestSources(DBTIntegrationTest):
 
     @use_profile('postgres')
     def test_basic_source_def(self):
+        os.environ['DBT_TEST_SCHEMA_NAME_VARIABLE'] = 'test_run_schema'
         self.run_sql_file('test/integration/042_sources_test/source.sql',
                           kwargs={'schema':self.unique_schema()})
         self.run_dbt([
