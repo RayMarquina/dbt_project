63047d01ab992de1c3af33739b4bf689aa2d8045
Jacob Beck <jake@fishtownanalytics.com>
2019-01-23 09:20:21 -0700

    add a very basic integration test for sources

---
+++ test/integration/042_sources_test/test_sources.py
@@ -0,0 +1,29 @@
+from nose.plugins.attrib import attr
+from test.integration.base import DBTIntegrationTest, use_profile
+
+
+class TestSources(DBTIntegrationTest):
+    @property
+    def schema(self):
+        return "sources_042"
+
+    @property
+    def models(self):
+        return "test/integration/042_sources_test/models"
+
+    @use_profile('postgres')
+    def test_basic_source_def(self):
+        self.run_sql_file('test/integration/042_sources_test/source.sql',
+                          kwargs={'schema':self.unique_schema()})
+        self.run_dbt([
+            'run',
+            '--vars',
+            '{{test_run_schema: {}}}'.format(self.unique_schema())
+        ])
+        self.assertTablesEqual('source', 'descendant_model')
+        results = self.run_dbt([
+            'test',
+            '--vars',
+            '{{test_run_schema: {}}}'.format(self.unique_schema())
+        ])
+        self.assertEqual(len(results), 2)
