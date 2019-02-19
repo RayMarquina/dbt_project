25d5fb16558f7de66a548149eec86745564450a6
Darren Haken <darrenhaken@gmail.com>
2019-02-19 12:18:09 +0000

    Add `persist_docs` to project level settings.
    Change `table_options` to have better error handling.

--- core/dbt/parser/source_config.py
+++ core/dbt/parser/source_config.py
@@ -7,7 +7,7 @@ from dbt.adapters.factory import get_adapter_class_by_name
 
 class SourceConfig(object):
     AppendListFields = {'pre-hook', 'post-hook', 'tags'}
-    ExtendDictFields = {'vars', 'column_types', 'quoting'}
+    ExtendDictFields = {'vars', 'column_types', 'quoting', 'persist_docs'}
     ClobberFields = {
         'alias',
         'schema',
