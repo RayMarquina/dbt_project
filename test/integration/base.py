wintersrd/dbt

fix this for real in a way that will make me not break it again

test/integration/base.py
@@ -445,8 +445,13 @@ class DBTIntegrationTest(unittest.TestCase):
         if args is None:
             args = ["run"]
 
+        prefix = []
         if strict:
-            args = ["--strict"] + args
+            prefix.append('--strict')
+        if os.getenv('DBT_TEST_SINGLE_THREADED') in ('y', 'Y', '1'):
+            prefix.append('--single-threaded')
+        args = prefix + args
+
         args.append('--log-cache-events')
         logger.info("Invoking dbt with {}".format(args))
 

RayMarquina/dbt_project

test/integration/base.py
@@ -451,6 +451,8 @@ class DBTIntegrationTest(unittest.TestCase):
             final_args.append('--strict')
         if parser:
             final_args.append('--test-new-parser')
+        if os.getenv('DBT_TEST_SINGLE_THREADED') in ('y', 'Y', '1'):
+            final_args.append('--single-threaded')
 
         final_args.extend(args)
         final_args.append('--log-cache-events')