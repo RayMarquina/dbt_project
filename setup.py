b62ba4a9854ba253919f0f065014908ea84959af
Drew Banin <drew@fishtownanalytics.com>
2019-05-10 10:52:57 -0400

    py2 compat

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 
