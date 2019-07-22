b6e7351431c2666e63d2acb7f79ac387ab7df658
Drew Banin <drew@fishtownanalytics.com>
2019-07-22 11:14:03 -0400

    snapshot surrogate key whitespace control

--- core/dbt/include/global_project/macros/materializations/snapshot/strategies.sql
+++ core/dbt/include/global_project/macros/materializations/snapshot/strategies.sql
@@ -41,10 +41,10 @@
 
 
 {% macro default__snapshot_hash_arguments(args) -%}
-    md5({% for arg in args %}
+    md5({%- for arg in args -%}
         coalesce(cast({{ arg }} as varchar ), '')
         {% if not loop.last %} || '|' || {% endif %}
-    {% endfor %})
+    {%- endfor -%})
 {%- endmacro %}
 
 
