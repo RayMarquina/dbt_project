786791670e667b35bf43561be00954440e9c2765
Connor McArthur <connor@fishtownanalytics.com>
2019-08-28 18:08:14 -0400

    Bump version: 0.14.1rc1 → 0.14.1rc2

--- core/dbt/version.py
+++ core/dbt/version.py
@@ -56,5 +56,5 @@ def get_version_information():
                 .format(version_msg))
 
 
-__version__ = '0.14.1rc1'
+__version__ = '0.14.1rc2'
 installed = get_installed_version()
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(this_directory, 'README.md')) as f:
 
 
 package_name = "dbt"
-package_version = "0.14.1rc1"
+package_version = "0.14.1rc2"
 description = """With dbt, data analysts and engineers can build analytics \
 the way engineers build applications."""
 
