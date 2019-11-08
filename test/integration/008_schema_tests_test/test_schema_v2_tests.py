5190b65b14fe206ac2846bc23a05ab7564c765df
Justin Larsen <justin@strala.com>
2019-11-07 17:54:43 -0700

    add error for empty test result set

--- test/integration/008_schema_tests_test/test_schema_v2_tests.py
+++ test/integration/008_schema_tests_test/test_schema_v2_tests.py
@@ -85,6 +85,51 @@ class TestMalformedSchemaTests(DBTIntegrationTest):
             self.run_dbt(strict=False)
 
 
+class TestMalformedMacroTests(DBTIntegrationTest):
+
+    def setUp(self):
+        DBTIntegrationTest.setUp(self)
+        self.run_sql_file("seed.sql")
+
+    @property
+    def schema(self):
+        return "schema_tests_008"
+    
+    @property
+    def models(self):
+        return "models-v2/custom-bad-test-macro"
+    
+    @property
+    def project_config(self):
+        return {
+            "macro-paths": ["macros-v2/malformed"],
+        }
+    
+    def run_schema_validations(self):
+        args = FakeArgs()
+        test_task = TestTask(args, self.config)
+        return test_task.run()
+
+    @use_profile('postgres')
+    def test_postgres_malformed_macro_reports_error(self):
+        self.run_dbt(["deps"])
+        self.run_dbt()
+        expected_failure = 'not_null'
+
+        test_results = self.run_schema_validations()
+
+        self.assertEqual(len(test_results), 2)
+
+        for result in test_results:
+            self.assertTrue(result.error is not None or result.fail)
+            # Assert that error is thrown for empty schema test
+            if result.error is not None:
+                self.assertIn("Returned 0 rows", result.error)
+            # Assert that failure occurs for normal schema test
+            elif result.fail:
+                self.assertIn(expected_failure, result.node.name)
+
+
 class TestHooksInTests(DBTIntegrationTest):
 
     @property
@@ -148,7 +193,7 @@ class TestCustomSchemaTests(DBTIntegrationTest):
         # dbt-integration-project contains a schema.yml file
         # both should work!
         return {
-            "macro-paths": ["macros-v2"],
+            "macro-paths": ["macros-v2/macros"],
         }
 
     @property
