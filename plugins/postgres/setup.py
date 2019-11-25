e51c942e91a94936f68f2965963d3b46f1257658
Connor McArthur <connor@fishtownanalytics.com>
2019-11-25 13:38:30 -0500

    Bump version: 0.15.0rc2 → 0.15.0

--- plugins/postgres/setup.py
+++ plugins/postgres/setup.py
@@ -25,7 +25,7 @@ def _dbt_psycopg2_name():
 
 
 package_name = "dbt-postgres"
-package_version = "0.15.0rc2"
+package_version = "0.15.0"
 description = """The postgres adpter plugin for dbt (data build tool)"""
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(this_directory, 'README.md')) as f:
 
 
 package_name = "dbt"
-package_version = "0.15.0rc2"
+package_version = "0.15.0"
 description = """With dbt, data analysts and engineers can build analytics \
 the way engineers build applications."""
 
