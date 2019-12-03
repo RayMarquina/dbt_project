9e9f0307df95eb2edfc7c6b3bf2716b23e4e144e
Kurt Convey <kconvey@verily.com>
2019-12-03 16:04:19 -0700

    newline and better test

--- test/integration/022_bigquery_test/models/sql_header_model.sql
+++ test/integration/022_bigquery_test/models/sql_header_model.sql
@@ -1,11 +1,8 @@
 {# This will fail if it is not extracted correctly #}
 {% call set_sql_header(config) %}
-  	create or replace table {{ ref('table_model') }}
-  	OPTIONS(
-		persist_docs={ "relation": true, "columns": true, "schema": true })
-  	as (
-		select * from {{ ref('view_model') }}
-  	  )
+  	create or replace table {{ ref('table_model') }} as (
+		select * from {{ ref('table_model') }}
+		)
 {% endcall %}
 
 select * from {{ ref('view_model') }}
