0d44dbf078bd5eb0813f351099f58de30a1ba934
Connor McArthur <connor@fishtownanalytics.com>
2020-02-02 14:16:13 -0500

    Bump version: 0.15.2a1 → 0.15.2

--- core/setup.py
+++ core/setup.py
@@ -18,7 +18,7 @@ def read(fname):
 
 
 package_name = "dbt-core"
-package_version = "0.15.2a1"
+package_version = "0.15.2"
 description = """dbt (data build tool) is a command line tool that helps \
 analysts and engineers transform data in their warehouse more effectively"""
 
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(os.path.join(this_directory, 'README.md')) as f:
 
 
 package_name = "dbt"
-package_version = "0.15.2a1"
+package_version = "0.15.2"
 description = """With dbt, data analysts and engineers can build analytics \
 the way engineers build applications."""
 
