f8b99f2e34c91bb39b9f6561cd9c163de9562d8c
Jacob Beck <jake@fishtownanalytics.com>
2020-03-20 11:31:17 -0600

    Bump version: 0.16.0rc3 → 0.16.0rc4

--- core/dbt/version.py
+++ core/dbt/version.py
@@ -56,5 +56,5 @@ def get_version_information():
                 .format(version_msg))
 
 
-__version__ = '0.16.0rc3'
+__version__ = '0.16.0rc4'
 installed = get_installed_version()
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(os.path.join(this_directory, 'README.md')) as f:
 
 
 package_name = "dbt"
-package_version = "0.16.0rc3"
+package_version = "0.16.0rc4"
 description = """With dbt, data analysts and engineers can build analytics \
 the way engineers build applications."""
 
