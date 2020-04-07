0a1862e92e62599b90602d2e43f172f7922948ed
Jacob Beck <jake@fishtownanalytics.com>
2020-04-07 14:15:04 -0600

    refactor source config

--- core/dbt/source_config.py
+++ core/dbt/source_config.py
@@ -1,4 +1,6 @@
-from typing import Dict, Any
+from dataclasses import dataclass
+from typing import Dict, Any, Mapping, List
+from typing_extensions import Protocol
 
 import dbt.exceptions
 
@@ -7,10 +9,31 @@ from dbt.node_types import NodeType
 from dbt.adapters.factory import get_adapter_class_by_name
 
 
-class SourceConfig:
+class HasConfigFields(Protocol):
+    seeds: Dict[str, Any]
+    snapshots: Dict[str, Any]
+    models: Dict[str, Any]
+
+
+class IsFQNResource(Protocol):
+    fqn: List[str]
+    resource_type: NodeType
+    package_name: str
+
+
+def _listify(value) -> List:
+    if isinstance(value, tuple):
+        value = list(value)
+    elif not isinstance(value, list):
+        value = [value]
+
+    return value
+
+
+class ConfigUpdater:
     AppendListFields = {'pre-hook', 'post-hook', 'tags'}
     ExtendDictFields = {'vars', 'column_types', 'quoting', 'persist_docs'}
-    ClobberFields = {
+    DefaultClobberFields = {
         'alias',
         'schema',
         'enabled',
@@ -32,31 +55,143 @@ class SourceConfig:
         # seeds
         'quote_columns',
     }
-    ConfigKeys = AppendListFields | ExtendDictFields | ClobberFields
 
-    def __init__(self, active_project, own_project, fqn, node_type):
-        self._config = None
-        # active_project is a RuntimeConfig, not a Project
-        self.active_project = active_project
-        self.own_project = own_project
-        self.fqn = fqn
-        self.node_type = node_type
+    @property
+    def ClobberFields(self):
+        return self.DefaultClobberFields | self.AdapterSpecificConfigs
 
-        adapter_type = active_project.credentials.type
+    @property
+    def ConfigKeys(self):
+        return (
+            self.AppendListFields | self.ExtendDictFields | self.ClobberFields
+        )
+
+    def __init__(self, adapter_type: str):
         adapter_class = get_adapter_class_by_name(adapter_type)
         self.AdapterSpecificConfigs = adapter_class.AdapterSpecificConfigs
 
-        # the config options defined within the model
-        self.in_model_config: Dict[str, Any] = {}
+    def update_config_keys_into(
+        self, mutable_config: Dict[str, Any], new_configs: Mapping[str, Any]
+    ) -> Dict[str, Any]:
+        """Update mutable_config with the contents of new_configs, but only
+        include "expected" config values.
 
-    def _merge(self, *configs):
+        Returns dict where the keys are what was updated and the update values
+        are what the updates were.
+        """
+
+        relevant_configs: Dict[str, Any] = {
+            key: new_configs[key] for key
+            in new_configs if key in self.ConfigKeys
+        }
+
+        for key in self.AppendListFields:
+            append_fields = _listify(relevant_configs.get(key, []))
+            mutable_config[key].extend([
+                f for f in append_fields if f not in mutable_config[key]
+            ])
+
+        for key in self.ExtendDictFields:
+            dict_val = relevant_configs.get(key, {})
+            try:
+                mutable_config[key].update(dict_val)
+            except (ValueError, TypeError, AttributeError):
+                dbt.exceptions.raise_compiler_error(
+                    'Invalid config field: "{}" must be a dict'.format(key)
+                )
+
+        for key in self.ClobberFields:
+            if key in relevant_configs:
+                mutable_config[key] = relevant_configs[key]
+
+        return relevant_configs
+
+    def update_into(
+        self, mutable_config: Dict[str, Any], new_config: Mapping[str, Any]
+    ) -> None:
+        """Update mutable_config with the contents of new_config."""
+        for key, value in new_config.items():
+            if key in self.AppendListFields:
+                current_list: List = _listify(mutable_config.get(key, []))
+                current_list.extend(_listify(value))
+                mutable_config[key] = current_list
+            elif key in self.ExtendDictFields:
+                current_dict: Dict = mutable_config.get(key, {})
+                try:
+                    current_dict.update(value)
+                except (ValueError, TypeError, AttributeError):
+                    dbt.exceptions.raise_compiler_error(
+                        'Invalid config field: "{}" must be a dict'.format(key)
+                    )
+                mutable_config[key] = current_dict
+            else:  # key in self.ClobberFields
+                mutable_config[key] = value
+
+    def get_project_config(
+        self, model: IsFQNResource, project: HasConfigFields
+    ) -> Dict[str, Any]:
+        # most configs are overwritten by a more specific config, but pre/post
+        # hooks are appended!
+        config: Dict[str, Any] = {}
+        for k in self.AppendListFields:
+            config[k] = []
+        for k in self.ExtendDictFields:
+            config[k] = {}
+
+        if model.resource_type == NodeType.Seed:
+            model_configs = project.seeds
+        elif model.resource_type == NodeType.Snapshot:
+            model_configs = project.snapshots
+        else:
+            model_configs = project.models
+
+        if model_configs is None:
+            return config
+
+        # mutates config
+        self.update_config_keys_into(config, model_configs)
+
+        fqn = model.fqn[:]
+        for level in fqn:
+            level_config = model_configs.get(level, None)
+            if level_config is None:
+                break
+
+            # mutates config
+            relevant_configs = self.update_config_keys_into(
+                config, level_config
+            )
+
+            # TODO: does this do anything? Doesn't update_config_keys_into
+            # handle the clobber case?
+            clobber_configs = {
+                k: v for (k, v) in relevant_configs.items()
+                if k not in self.AppendListFields and
+                k not in self.ExtendDictFields
+            }
+
+            config.update(clobber_configs)
+            model_configs = model_configs[level]
+
+        return config
+
+    def get_project_vars(
+        self, project_vars: Dict[str, Any],
+    ):
+        config: Dict[str, Any] = {}
+        # this is pretty trivial, since the new project vars don't care about
+        # FQNs or resource types
+        self.update_config_keys_into(config, project_vars)
+        return config
+
+    def merge(self, *configs: Dict[str, Any]) -> Dict[str, Any]:
         merged_config: Dict[str, Any] = {}
         for config in configs:
             # Do not attempt to deep merge clobber fields
             config = config.copy()
             clobber = {
                 key: config.pop(key) for key in list(config.keys())
-                if key in (self.ClobberFields | self.AdapterSpecificConfigs)
+                if key in self.ClobberFields
             }
             intermediary_merged = deep_merge(
                 merged_config, config
@@ -65,6 +200,41 @@ class SourceConfig:
 
             merged_config.update(intermediary_merged)
         return merged_config
+
+
+@dataclass
+class ModelParts:
+    fqn: List[str]
+    resource_type: NodeType
+
+
+class SourceConfig:
+    def __init__(self, active_project, own_project, fqn, node_type):
+        self._config = None
+        # active_project is a RuntimeConfig, not a Project
+        self.active_project = active_project
+        self.own_project = own_project
+
+        # TODO: have this __init__ just take a `IsFQNResource` parameter?
+        self.model = ModelParts(fqn=fqn, resource_type=node_type)
+
+        self.updater = ConfigUpdater(active_project.credentials.type)
+
+        # the config options defined within the model
+        self.in_model_config: Dict[str, Any] = {}
+
+    def get_default(self) -> Dict[str, Any]:
+        defaults = {"enabled": True, "materialized": "view"}
+
+        if self.model.resource_type == NodeType.Seed:
+            defaults['materialized'] = 'seed'
+        elif self.model.resource_type == NodeType.Snapshot:
+            defaults['materialized'] = 'snapshot'
+
+        if self.model.resource_type == NodeType.Test:
+            defaults['severity'] = 'ERROR'
+
+        return defaults
 
     # this is re-evaluated every time `config` is called.
     # we can cache it, but that complicates things.
@@ -83,25 +253,17 @@ class SourceConfig:
            - in-model config
         """
 
-        defaults = {"enabled": True, "materialized": "view"}
-
-        if self.node_type == NodeType.Seed:
-            defaults['materialized'] = 'seed'
-        elif self.node_type == NodeType.Snapshot:
-            defaults['materialized'] = 'snapshot'
-
-        if self.node_type == NodeType.Test:
-            defaults['severity'] = 'ERROR'
-
+        defaults = self.get_default()
         active_config = self.load_config_from_active_project()
 
         if self.active_project.project_name == self.own_project.project_name:
-            cfg = self._merge(defaults, active_config,
-                              self.in_model_config)
+            cfg = self.updater.merge(
+                defaults, active_config, self.in_model_config
+            )
         else:
             own_config = self.load_config_from_own_project()
 
-            cfg = self._merge(
+            cfg = self.updater.merge(
                 defaults, own_config, self.in_model_config, active_config
             )
 
@@ -110,111 +272,12 @@ class SourceConfig:
     def _translate_adapter_aliases(self, config):
         return self.active_project.credentials.translate_aliases(config)
 
-    def update_in_model_config(self, config):
+    def update_in_model_config(self, config) -> None:
         config = self._translate_adapter_aliases(config)
-        for key, value in config.items():
-            if key in self.AppendListFields:
-                current = self.in_model_config.get(key, [])
-                if not isinstance(value, (list, tuple)):
-                    value = [value]
-                current.extend(value)
-                self.in_model_config[key] = current
-            elif key in self.ExtendDictFields:
-                current = self.in_model_config.get(key, {})
-                try:
-                    current.update(value)
-                except (ValueError, TypeError, AttributeError):
-                    dbt.exceptions.raise_compiler_error(
-                        'Invalid config field: "{}" must be a dict'.format(key)
-                    )
-                self.in_model_config[key] = current
-            else:  # key in self.ClobberFields or self.AdapterSpecificConfigs
-                self.in_model_config[key] = value
+        self.updater.update_into(self.in_model_config, config)
 
-    @staticmethod
-    def __get_as_list(relevant_configs, key):
-        if key not in relevant_configs:
-            return []
+    def load_config_from_own_project(self) -> Dict[str, Any]:
+        return self.updater.get_project_config(self.model, self.own_project)
 
-        items = relevant_configs[key]
-        if not isinstance(items, (list, tuple)):
-            items = [items]
-
-        return items
-
-    def smart_update(self, mutable_config, new_configs):
-        config_keys = self.ConfigKeys | self.AdapterSpecificConfigs
-
-        relevant_configs: Dict[str, Any] = {
-            key: new_configs[key] for key
-            in new_configs if key in config_keys
-        }
-
-        for key in self.AppendListFields:
-            append_fields = self.__get_as_list(relevant_configs, key)
-            mutable_config[key].extend([
-                f for f in append_fields if f not in mutable_config[key]
-            ])
-
-        for key in self.ExtendDictFields:
-            dict_val = relevant_configs.get(key, {})
-            try:
-                mutable_config[key].update(dict_val)
-            except (ValueError, TypeError, AttributeError):
-                dbt.exceptions.raise_compiler_error(
-                    'Invalid config field: "{}" must be a dict'.format(key)
-                )
-
-        for key in (self.ClobberFields | self.AdapterSpecificConfigs):
-            if key in relevant_configs:
-                mutable_config[key] = relevant_configs[key]
-
-        return relevant_configs
-
-    def get_project_config(self, runtime_config):
-        # most configs are overwritten by a more specific config, but pre/post
-        # hooks are appended!
-        config: Dict[str, Any] = {}
-        for k in self.AppendListFields:
-            config[k] = []
-        for k in self.ExtendDictFields:
-            config[k] = {}
-
-        if self.node_type == NodeType.Seed:
-            model_configs = runtime_config.seeds
-        elif self.node_type == NodeType.Snapshot:
-            model_configs = runtime_config.snapshots
-        else:
-            model_configs = runtime_config.models
-
-        if model_configs is None:
-            return config
-
-        # mutates config
-        self.smart_update(config, model_configs)
-
-        fqn = self.fqn[:]
-        for level in fqn:
-            level_config = model_configs.get(level, None)
-            if level_config is None:
-                break
-
-            # mutates config
-            relevant_configs = self.smart_update(config, level_config)
-
-            clobber_configs = {
-                k: v for (k, v) in relevant_configs.items()
-                if k not in self.AppendListFields and
-                k not in self.ExtendDictFields
-            }
-
-            config.update(clobber_configs)
-            model_configs = model_configs[level]
-
-        return config
-
-    def load_config_from_own_project(self):
-        return self.get_project_config(self.own_project)
-
-    def load_config_from_active_project(self):
-        return self.get_project_config(self.active_project)
+    def load_config_from_active_project(self) -> Dict[str, Any]:
+        return self.updater.get_project_config(self.model, self.active_project)
