fd4a33e450cb217c876452d324a061dcdc6fc6a4
Jacob Beck <jake@fishtownanalytics.com>
2020-04-08 12:03:07 -0600

    Fix merge: it doubled up a few lines

--- core/dbt/contracts/connection.py
+++ core/dbt/contracts/connection.py
@@ -201,32 +201,6 @@ class QueryComment(JsonSchemaMixin):
     append: bool = False
 
 
-DEFAULT_QUERY_COMMENT = '''
-{%- set comment_dict = {} -%}
-{%- do comment_dict.update(
-    app='dbt',
-    dbt_version=dbt_version,
-    profile_name=target.get('profile_name'),
-    target_name=target.get('target_name'),
-) -%}
-{%- if node is not none -%}
-  {%- do comment_dict.update(
-    node_id=node.unique_id,
-  ) -%}
-{% else %}
-  {# in the node context, the connection name is the node_id #}
-  {%- do comment_dict.update(connection_name=connection_name) -%}
-{%- endif -%}
-{{ return(tojson(comment_dict)) }}
-'''
-
-
-@dataclass
-class QueryComment(JsonSchemaMixin):
-    comment: str = DEFAULT_QUERY_COMMENT
-    append: bool = False
-
-
 class AdapterRequiredConfig(HasCredentials, Protocol):
     project_name: str
     query_comment: QueryComment
