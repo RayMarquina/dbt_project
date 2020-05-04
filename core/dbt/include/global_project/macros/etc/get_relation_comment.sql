0fdff04fc6552363ecbfbdd4dc9602a6a2e37dd1
Andrew Seitz <andrew.seitz@snowflake.com>
2020-05-04 14:48:55 -0700

    fixing rebase

--- core/dbt/include/global_project/macros/etc/get_relation_comment.sql
+++ core/dbt/include/global_project/macros/etc/get_relation_comment.sql
@@ -1,7 +1,7 @@
 {% macro get_relation_comment(persist_docs, model) %}
 
   {%- if persist_docs is not mapping -%}
-    {{ exceptions.raise_compiler_error("Invalid value provided for 'persist_docs'. Expected dict but got value: " ~ raw_persist_docs) }}
+    {{ exceptions.raise_compiler_error("Invalid value provided for 'persist_docs'. Expected dict but got value: " ~ persist_docs) }}
   {% endif %}
 
   {% if persist_docs.get('relation', false) %}
@@ -13,25 +13,10 @@
 {% endmacro %}
 
 
-{# copy+pasted from the snowflake PR - delete this on merge #}
 {% macro get_relation_column_comments(persist_docs, model) %}
+
   {%- if persist_docs is not mapping -%}
     {{ exceptions.raise_compiler_error("Invalid value provided for 'persist_docs'. Expected dict but got value: " ~ persist_docs) }}
-  {% endif %}
-
-  {% if persist_docs.get('columns', false) and model.columns|length != 0 %}
-    {{ return(model.columns) }}
-  {%- else -%}
-    {{ return(none) }}
-  {% endif %}
-
-{% endmacro %}
-
-
-{% macro get_relation_column_comments(persist_docs, model) %}
-
-  {%- if persist_docs is not mapping -%}
-    {{ exceptions.raise_compiler_error("Invalid value provided for 'persist_docs'. Expected dict but got value: " ~ raw_persist_docs) }}
   {% endif %}
 
   {% if persist_docs.get('columns', false) and model.columns|length != 0 %}
