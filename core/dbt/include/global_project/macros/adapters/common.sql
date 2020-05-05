b774702ee826594f0cb6f0ef5824492dbc0333ab
Andrew Seitz <andrew.seitz@snowflake.com>
2020-05-05 10:21:36 -0700

    removing duplicates in common

--- core/dbt/include/global_project/macros/adapters/common.sql
+++ core/dbt/include/global_project/macros/adapters/common.sql
@@ -323,22 +323,3 @@
   {%- endif -%}
 {%- endmacro -%}
 
-
-{# copy+pasted from the snowflake PR - delete these 4 on merge #}
-{% macro alter_column_comment(relation, column_dict) -%}
-  {{ return(adapter_macro('alter_column_comment', relation, column_dict)) }}
-{% endmacro %}
-
-{% macro default__alter_column_comment(relation, column_dict) -%}
-  {{ exceptions.raise_not_implemented(
-    'alter_column_comment macro not implemented for adapter '+adapter.type()) }}
-{% endmacro %}
-
-{% macro alter_relation_comment(relation, relation_comment) -%}
-  {{ return(adapter_macro('alter_relation_comment', relation, relation_comment)) }}
-{% endmacro %}
-
-{% macro default__alter_relation_comment(relation, relation_comment) -%}
-  {{ exceptions.raise_not_implemented(
-    'alter_relation_comment macro not implemented for adapter '+adapter.type()) }}
-{% endmacro %}
