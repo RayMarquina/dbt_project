451e95e48d3b6a0d51c90f6e707f3ad9ef51efbe
Jacob Beck <jake@fishtownanalytics.com>
2020-05-12 10:45:59 -0600

    Bump version: 0.17.0b1 → 0.17.0rc1

--- core/setup.py
+++ core/setup.py
@@ -18,7 +18,7 @@ def read(fname):
 
 
 package_name = "dbt-core"
-package_version = "0.17.0b1"
+package_version = "0.17.0rc1"
 description = """dbt (data build tool) is a command line tool that helps \
 analysts and engineers transform data in their warehouse more effectively"""
 
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(os.path.join(this_directory, 'README.md')) as f:
 
 
 package_name = "dbt"
-package_version = "0.17.0b1"
+package_version = "0.17.0rc1"
 description = """With dbt, data analysts and engineers can build analytics \
 the way engineers build applications."""
 
