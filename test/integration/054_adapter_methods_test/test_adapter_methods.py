7aa09e393c705a89d841386ec386e00b882c4301
Azhar Dewji <azhardewji@gmail.com>
2020-06-09 14:50:08 -0400

    Undo accidental commented out test

--- test/integration/054_adapter_methods_test/test_adapter_methods.py
+++ test/integration/054_adapter_methods_test/test_adapter_methods.py
@@ -2,45 +2,45 @@ from test.integration.base import DBTIntegrationTest, use_profile
 import yaml
 
 
-# class TestBaseCaching(DBTIntegrationTest):
-#     @property
-#     def schema(self):
-#         return "caching_038"
-#
-#     @property
-#     def models(self):
-#         return "models"
-#
-#     @property
-#     def project_config(self):
-#         return {
-#             'config-version': 2,
-#             'test-paths': ['tests']
-#         }
-#
-#     @use_profile('postgres')
-#     def test_postgres_adapter_methods(self):
-#         self.run_dbt(['compile'])  # trigger any compile-time issues
-#         self.run_dbt()
-#         self.assertTablesEqual('model', 'expected')
-#
-#     @use_profile('redshift')
-#     def test_redshift_adapter_methods(self):
-#         self.run_dbt(['compile'])  # trigger any compile-time issues
-#         self.run_dbt()
-#         self.assertTablesEqual('model', 'expected')
-#
-#     @use_profile('snowflake')
-#     def test_snowflake_adapter_methods(self):
-#         self.run_dbt(['compile'])  # trigger any compile-time issues
-#         self.run_dbt()
-#         self.assertTablesEqual('MODEL', 'EXPECTED')
-#
-#     @use_profile('bigquery')
-#     def test_bigquery_adapter_methods(self):
-#         self.run_dbt(['compile'])  # trigger any compile-time issues
-#         self.run_dbt()
-#         self.assertTablesEqual('model', 'expected')
+class TestBaseCaching(DBTIntegrationTest):
+    @property
+    def schema(self):
+        return "caching_038"
+
+    @property
+    def models(self):
+        return "models"
+
+    @property
+    def project_config(self):
+        return {
+            'config-version': 2,
+            'test-paths': ['tests']
+        }
+
+    @use_profile('postgres')
+    def test_postgres_adapter_methods(self):
+        self.run_dbt(['compile'])  # trigger any compile-time issues
+        self.run_dbt()
+        self.assertTablesEqual('model', 'expected')
+
+    @use_profile('redshift')
+    def test_redshift_adapter_methods(self):
+        self.run_dbt(['compile'])  # trigger any compile-time issues
+        self.run_dbt()
+        self.assertTablesEqual('model', 'expected')
+
+    @use_profile('snowflake')
+    def test_snowflake_adapter_methods(self):
+        self.run_dbt(['compile'])  # trigger any compile-time issues
+        self.run_dbt()
+        self.assertTablesEqual('MODEL', 'EXPECTED')
+
+    @use_profile('bigquery')
+    def test_bigquery_adapter_methods(self):
+        self.run_dbt(['compile'])  # trigger any compile-time issues
+        self.run_dbt()
+        self.assertTablesEqual('model', 'expected')
 
 
 class TestRenameRelation(DBTIntegrationTest):
