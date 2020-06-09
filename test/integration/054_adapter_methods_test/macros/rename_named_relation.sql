107170163a46cec5d78ddab6b26b1880fdd2b81e
Azhar Dewji <azhardewji@gmail.com>
2020-06-09 15:00:29 -0400

    Fixed relation creation

--- test/integration/054_adapter_methods_test/macros/rename_named_relation.sql
+++ test/integration/054_adapter_methods_test/macros/rename_named_relation.sql
@@ -1,6 +1,6 @@
 -- Macro to rename a relation
 {% macro rename_named_relation(from_name, to_name) %}
-{%- set from_relation = adapter.get_relation(database=target.database, schema=target.schema, identifier=from_name) -%}
-{%- set to_relation = adapter.get_relation(database=target.database, schema=target.schema, identifier=to_name) -%}
+{%- set from_relation = api.Relation.create(database=target.database, schema=target.schema, identifier=from_name, type='table') -%}
+{%- set to_relation = api.Relation.create(database=target.database, schema=target.schema, identifier=to_name, type='table') -%}
 {% do adapter.rename_relation(from_relation, to_relation) %}
 {% endmacro %}
