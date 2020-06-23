d38fd80dff38a9a58f45eac9b13ce061840ff82c
Jacob Beck <jake@fishtownanalytics.com>
2020-06-23 13:25:01 -0600

    PR feedback: double seeds

--- core/dbt/config/renderer.py
+++ core/dbt/config/renderer.py
@@ -78,7 +78,7 @@ class DbtProjectYamlRenderer(BaseRenderer):
         if first in {'on-run-start', 'on-run-end', 'query-comment'}:
             return False
         # models have two things to avoid
-        if first in {'seeds', 'models', 'snapshots', 'seeds'}:
+        if first in {'seeds', 'models', 'snapshots'}:
             # model-level hooks
             if 'pre-hook' in keypath or 'post-hook' in keypath:
                 return False
