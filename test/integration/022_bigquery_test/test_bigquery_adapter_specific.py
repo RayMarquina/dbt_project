099fea8565d78a7d8f9c4cca05093214db97229f
Kurt Convey <kconvey@verily.com>
2020-08-17 15:04:59 -0600

    Add integration test

---
+++ test/integration/022_bigquery_test/test_bigquery_adapter_specific.py
@@ -0,0 +1,37 @@
+""""Test adapter specific config options."""
+from test.integration.base import DBTIntegrationTest, use_profile
+import textwrap
+import yaml
+
+
+class TestBigqueryDatePartitioning(DBTIntegrationTest):
+
+    @property
+    def schema(self):
+        return "bigquery_test_022"
+
+    @property
+    def models(self):
+        return "adapter-specific-models"
+
+    @property
+    def profile_config(self):
+        return self.bigquery_profile()
+
+    @property
+    def project_config(self):
+        return yaml.safe_load(textwrap.dedent('''\
+        config-version: 2
+        models:
+            test:
+                materialized: table
+                expiring_table:
+                    time_to_expiration: 4    
+        '''))
+
+    @use_profile('bigquery')
+    def test_bigquery_location_invalid(self):
+        _, stdout = self.run_dbt_and_capture()
+        self.assertIn(
+            'expiration_timestamp: TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL '
+            '4 hour)', stdout)
