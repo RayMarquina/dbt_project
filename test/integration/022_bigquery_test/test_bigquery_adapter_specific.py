383480592964f5b1f9fd0bb31672464d94ee46e5
Kurt Convey <kconvey@verily.com>
2020-08-18 10:25:11 -0600

    Use injected sql from results

--- test/integration/022_bigquery_test/test_bigquery_adapter_specific.py
+++ test/integration/022_bigquery_test/test_bigquery_adapter_specific.py
@@ -31,7 +31,8 @@ class TestBigqueryAdapterSpecific(DBTIntegrationTest):
 
     @use_profile('bigquery')
     def test_bigquery_time_to_expiration(self):
-        _, stdout = self.run_dbt_and_capture()
+        results = self.run_dbt()
+        
         self.assertIn(
             'expiration_timestamp: TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL '
-            '4 hour)', stdout)
+            '4 hour)', results[0].node.injected_sql)
