eff198d079a9b8f41ab9b4d4bbf474772367024b
Ben Edwards <ben.edwards@benjaminsedwards.com>
2020-11-03 20:43:46 +1100

    Add integration tests

--- test/integration/063_relation_name_tests/test_relation_name.py
+++ test/integration/063_relation_name_tests/test_relation_name.py
@@ -1,11 +1,12 @@
 from test.integration.base import DBTIntegrationTest, use_profile
+from pytest import mark
 import dbt.exceptions
 
-class TestAdapterDDL(DBTIntegrationTest):
 
+class TestAdapterDDL(DBTIntegrationTest):
     def setUp(self):
         DBTIntegrationTest.setUp(self)
-        self.run_dbt(['seed'])
+        self.run_dbt(["seed"])
 
     @property
     def schema(self):
@@ -18,17 +19,61 @@ class TestAdapterDDL(DBTIntegrationTest):
     @property
     def project_config(self):
         return {
-            'config-version': 2,
-            'seeds': {
-                'quote_columns': False,
+            "config-version": 2,
+            "seeds": {
+                "quote_columns": False,
             },
         }
 
-    @use_profile('postgres')
-    def test_postgres_long_name_fails(self):
-        self.run_dbt(['run'],expect_pass=False)
+    # 63 characters is the character limit for a table name in a postgres database
+    # (assuming compiled without changes from source)
+    @use_profile("postgres")
+    def test_postgres_name_longer_than_63_fails(self):
+        self.run_dbt(
+            [
+                "run",
+                "-m",
+                "my_name_is_64_characters_abcdefghijklmnopqrstuvwxyz0123456789012",
+            ],
+            expect_pass=False,
+        )
 
-    @use_profile('redshift')
+    @mark.skip(
+        reason="Backup table generation currently adds 12 characters to the relation name, meaning the current name limit is 51."
+    )
+    @use_profile("postgres")
+    def test_postgres_name_shorter_or_equal_to_63_passes(self):
+        self.run_dbt(
+            [
+                "run",
+                "-m",
+                "my_name_is_52_characters_abcdefghijklmnopqrstuvwxyz0"
+                "my_name_is_63_characters_abcdefghijklmnopqrstuvwxyz012345678901",
+            ],
+            expect_pass=True,
+        )
+
+    @use_profile("postgres")
+    def test_postgres_long_name_passes_when_temp_tables_are_generated(self):
+        self.run_dbt(
+            [
+                "run",
+                "-m",
+                "my_name_is_51_characters_incremental_abcdefghijklmn",
+            ],
+            expect_pass=True,
+        )
+
+        # Run again to trigger incremental materialization
+        self.run_dbt(
+            [
+                "run",
+                "-m",
+                "my_name_is_51_characters_incremental_abcdefghijklmn",
+            ],
+            expect_pass=True,
+        )
+
+    @use_profile("redshift")
     def test_redshift_long_name_succeeds(self):
-        self.run_dbt(['run'],expect_pass=True)
-
+        self.run_dbt(["run"], expect_pass=True)
