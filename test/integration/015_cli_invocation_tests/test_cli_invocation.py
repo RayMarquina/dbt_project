1a8416c2972797dc1b874acaf47c9aabb3f2b25d
Gerda Shank <gerda@fishtownanalytics.com>
2020-11-12 13:56:41 -0500

    Add parse command and collect parse timing info [#2824]

--- test/integration/015_cli_invocation_tests/test_cli_invocation.py
+++ test/integration/015_cli_invocation_tests/test_cli_invocation.py
@@ -168,6 +168,7 @@ class TestCLIInvocationWithProjectDir(ModelCopyingIntegrationTest):
         self.run_dbt(['seed', '--project-dir', project_dir])
         self.run_dbt(['run', '--project-dir', project_dir])
         self.run_dbt(['test', '--project-dir', project_dir])
+        self.run_dbt(['parse', '--project-dir', project_dir])
         self.run_dbt(['clean', '--project-dir', project_dir])
         # In case of 'dbt clean' also test that the clean-targets directories were deleted.
         for target in self.config.clean_targets:
