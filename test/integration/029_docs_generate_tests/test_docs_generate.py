c4c5b593128f3c4406e46742d3d0d93544dac296
Joel Labes <joel.labes@educationperfect.com>
2020-12-07 17:45:12 +1300

    Stab at updating parent and child maps

--- test/integration/029_docs_generate_tests/test_docs_generate.py
+++ test/integration/029_docs_generate_tests/test_docs_generate.py
@@ -2110,8 +2110,9 @@ class TestDocsGenerate(DBTIntegrationTest):
             },
             'child_map': {
                 'model.test.ephemeral_copy': ['model.test.ephemeral_summary'],
+		        'exposure.test.notebook_exposure': ['model.test.view_summary'],
                 'model.test.ephemeral_summary': ['model.test.view_summary'],
-                'model.test.view_summary': [],
+                'model.test.view_summary': ['exposure.test.notebook_exposure'],
                 'seed.test.seed': ['snapshot.test.snapshot_seed'],
                 'snapshot.test.snapshot_seed': [],
                 'source.test.my_source.my_table': ['model.test.ephemeral_copy'],
@@ -2120,6 +2121,7 @@ class TestDocsGenerate(DBTIntegrationTest):
                 'model.test.ephemeral_copy': ['source.test.my_source.my_table'],
                 'model.test.ephemeral_summary': ['model.test.ephemeral_copy'],
                 'model.test.view_summary': ['model.test.ephemeral_summary'],
+                'exposure.test.notebook_exposure': ['model.test.view_summary'],
                 'seed.test.seed': [],
                 'snapshot.test.snapshot_seed': ['seed.test.seed'],
                 'source.test.my_source.my_table': [],
