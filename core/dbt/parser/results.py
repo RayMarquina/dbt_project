31989b85d183fa93e4ae5697008c7796cd61ee1a
Chandrakant Gopalan <chandrakant.gopalan@gmail.com>
2021-03-14 15:46:47 -0400

    Fix flake8 errors

--- core/dbt/parser/results.py
+++ core/dbt/parser/results.py
@@ -142,10 +142,11 @@ class ParseResult(dbtClassMixin, Writable, Replaceable):
             )
             dup_path_msg = line_wrap_message(
                 f"""\
-                The file {macro.original_file_path} was parsed multiple times by dbt.
-                This error happens when a path is duplicated in a dbt_project.yml configuration.
-                Check your `dbt_project.yml` file path configurations and remove any
-                duplicated paths to fix this error
+                The file {macro.original_file_path} was parsed multiple times
+                by dbt. This error happens when a path is duplicated in a
+                dbt_project.yml configuration.
+                Check your `dbt_project.yml` file path configurations and
+                remove any duplicated paths to fix this error
                 """,
                 subtract=2,
             )
