909068dfa8f74903c774bb5e42f81946baf8e473
Prratek Ramchandani <34848565+prratek@users.noreply.github.com>
2021-03-16 15:53:25 -0400

    leave quoting for merge_update_columns to the user
    Co-authored-by: Jeremy Cohen <jtcohen6@gmail.com>

--- core/dbt/include/global_project/macros/materializations/common/merge.sql
+++ core/dbt/include/global_project/macros/materializations/common/merge.sql
@@ -18,7 +18,7 @@
 {% macro default__get_merge_sql(target, source, unique_key, dest_columns, predicates) -%}
     {%- set predicates = [] if predicates is none else [] + predicates -%}
     {%- set dest_cols_csv = get_quoted_csv(dest_columns | map(attribute="name")) -%}
-    {%- set update_columns = config.get('merge_update_columns', default = dest_columns | map(attribute="name") | list) -%}
+    {%- set update_columns = config.get('merge_update_columns', default = dest_columns | map(attribute="quoted") | list) -%}
     {%- set sql_header = config.get('sql_header', none) -%}
 
     {% if unique_key %}
