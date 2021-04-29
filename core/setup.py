a76ec42586e9c06cf2fee82fa1359643ec4b8150
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2021-04-29 10:33:06 -0400

    static parsing skateboard

--- core/setup.py
+++ core/setup.py
@@ -69,10 +69,8 @@ setup(
         'json-rpc>=1.12,<2',
         'logbook>=1.5,<1.6',
         'mashumaro==2.0',
-        'minimal-snowplow-tracker==0.0.2',
-        'networkx>=2.3,<3',
-        'packaging~=20.9',
-        'sqlparse>=0.2.3,<0.4',
+        'tree-sitter==0.19.0',
+        'tree-sitter-jinja2==0.1.0a1',
         'typing-extensions>=3.7.4,<3.8',
         'werkzeug>=0.15,<2.0',
         # the following are all to match snowflake-connector-python
