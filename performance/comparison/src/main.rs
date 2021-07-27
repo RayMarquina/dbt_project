4ddba7e44c6b232e5348337fd4dc8b70742785cc
Kyle Wigley <kyle@fishtownanalytics.com>
2021-07-27 12:44:06 -0400

    --wip--

--- performance/comparison/src/main.rs
+++ performance/comparison/src/main.rs
@@ -26,18 +26,25 @@ struct Measurements {
 struct Regression {
     threshold: f64,
     difference: f64,
-    base: Measurement,
+    baseline: Measurement,
     latest: Measurement,
 }
 
-fn regression(base: &Measurement, latest: &Measurement) -> Option<Regression> {
+#[derive(Debug, Clone, PartialEq)]
+struct MeasurementGroup {
+    version: String,
+    run: String,
+    measurement: Measurement,
+}
+
+fn regression(baseline: &Measurement, latest: &Measurement) -> Option<Regression> {
     let threshold = 1.05; // 5% regression threshold
-    let difference = latest.median / base.median;
+    let difference = latest.median / baseline.median;
     if difference > threshold {
         Some(Regression {
             threshold,
             difference,
-            base: base.clone(),
+            baseline: baseline.clone(),
             latest: latest.clone(),
         })
     } else {
@@ -45,11 +52,30 @@ fn regression(base: &Measurement, latest: &Measurement) -> Option<Regression> {
     }
 }
 
-#[derive(Debug, Clone, PartialEq)]
-struct MeasurementGroup {
-    branch: String,
-    run: String,
-    measurement: Measurement,
+fn measurements_from_files(
+    results_directory: &Path,
+) -> std::result::Result<Vec<Measurements>, std::io::Error> {
+    let result_files = fs::read_dir(results_directory)?;
+
+    result_files
+        .into_iter()
+        .map(|f| f.unwrap().path())
+        .filter(|filename| {
+            filename
+                .extension()
+                .and_then(|ext| ext.to_str())
+                .map_or(false, |ext| ext.ends_with("json"))
+        })
+        .map(|filename| {
+            println!("{:?}", filename);
+            let contents = fs::read_to_string(&filename);
+            contents.map(|c| serde_json::from_str::<Measurements>(&c).unwrap()) //TODO rm unwrap!
+        })
+        .collect()
+}
+
+fn detect_regressions(measurements: Vec<Measurements>) -> Vec<Regression> {
+    panic!()
 }
 
 fn main() {
@@ -61,46 +87,50 @@ fn main() {
     }
     let results_directory = &args[1];
 
-    let path = Path::new(&results_directory);
-    let result_files = fs::read_dir(path).unwrap();
-    let measurements: Result<Vec<MeasurementGroup>> = result_files
-        .into_iter()
-        .map(|f| f.unwrap().path())
-        .filter(|filename| {
-            filename
-                .extension()
-                .and_then(|ext| ext.to_str())
-                .map_or(false, |ext| ext.ends_with("json"))
-        })
-        .map(|filename| {
-            println!("{:?}", filename);
-            let contents = fs::read_to_string(&filename).unwrap();
-            let measurements: Result<Measurements> = serde_json::from_str(&contents);
-            let results = measurements.map(|x| x.results[0].clone());
-            let filepath = filename.into_os_string().into_string().unwrap();
-            let parts: Vec<&str> = filepath.split("_").collect();
+    let foo = measurements_from_files(Path::new(&results_directory));
+    foo.map(|x| detect_regressions(x));
 
-            // the way we're running these, the files will each contain exactly one measurement
-            results.map(|r| MeasurementGroup {
-                branch: parts[0].to_owned(),
-                run: parts[1..].join(""),
-                measurement: r.clone(),
-            })
-        })
-        .collect();
+    // let measurements: Result<Vec<(&str, Vec<MeasurementGroup>)>> = result_files
+    //     .into_iter()
+    //     .map(|f| f.unwrap().path())
+    //     .filter(|filename| {
+    //         filename
+    //             .extension()
+    //             .and_then(|ext| ext.to_str())
+    //             .map_or(false, |ext| ext.ends_with("json"))
+    //     })
+    //     .map(|filename| {
+    //         println!("{:?}", filename);
+    //         let contents = fs::read_to_string(&filename).unwrap();
+    //         let measurements: Result<Measurements> = serde_json::from_str(&contents);
+    //         let results = measurements.map(|x| x.results[0].clone());
+    //         let filepath = filename.into_os_string().into_string().unwrap();
+    //         let parts: Vec<&str> = filepath.split("_").collect();
+
+    //         // the way we're running these, the files will each contain exactly one measurement
+    //         results.map(|r| MeasurementGroup {
+    //             version: parts[0].to_owned(),
+    //             run: parts[1..].join(""),
+    //             measurement: r.clone(),
+    //         })
+    //     })
+    //     .collect();
 
     // TODO
     // - group by project and metric
     // - each group will have 2 measurements for each branch
     // - perform regressions on each pair
-    // dev_parse_02_mini_project_dont_merge.json
-    // dev_parse_01_mini_project_dont_merge.json
+    // latest_parse_02_mini_project_dont_merge.json
+    // latest_parse_01_mini_project_dont_merge.json
+    // baseline_parse_02_mini_project_dont_merge.json
+    // baseline_parse_01_mini_project_dont_merge.json
+    // command, project -> 2 version
+    // let x: Vec<(&str, Vec<MeasurementGroup>)>;
 
     // TODO exit(1) when Regression is present
-    match measurements {
-        Err(e) => panic!("{}", e),
-        Ok(_) => (),
-    }
-
-    println!("{:?}", measurements);
+    // match foo {
+    //     Err(&e) => panic!("{}", &e),
+    //     Ok(_) => (),
+    // }
+    foo.clone().map(|f| println!("{:?}", f)).unwrap();
 }
