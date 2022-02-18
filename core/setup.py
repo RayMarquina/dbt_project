a66fe7f4674eb5dcb1dcfb34652c4975da9eaa5a
Jeremy Cohen <jeremy@dbtlabs.com>
2022-02-18 14:35:27 +0100

    Pin MarkupSafe==2.0.1 (#4746)

--- core/setup.py
+++ core/setup.py
@@ -52,6 +52,7 @@ setup(
     ],
     install_requires=[
         "Jinja2==2.11.3",
+        "MarkupSafe==2.0.1",
         "agate>=1.6,<1.6.4",
         "click>=7.0,<9",
         "colorama>=0.3.9,<0.4.5",
